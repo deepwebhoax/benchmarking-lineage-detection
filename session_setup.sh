26d932e5ce5c7c55d2949228831f264eccfd1952
Taylor Shabani <taylorsh@login2.hoffman2.idre.ucla.edu>
2022-08-06 15:27:32 -0700

    ADD: CliequeSNV results, envi setup and WIV04 reference

---
+++ session_setup.sh
@@ -0,0 +1,7 @@
+export JAVA_OPTS="-Xmx2048m -XX:CompressedClassSpaceSize=256m"
+export _JAVA_OPTIONS="-Xmx2048m -XX:CompressedClassSpaceSize=256m"
+export PATH="/u/home/t/taylorsh/Bohdan/sam1/bin:$PATH"
+export PATH="/u/home/t/taylorsh/Bohdan/PredictHaplo-2.1.4:$PATH"
+export PATH="/u/home/t/taylorsh/Bohdan/aBayesQR:$PATH" 
+echo "setup complete!"
+
